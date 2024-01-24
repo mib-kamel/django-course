91d229b09983fde301d8c8fc15e33174875604da
Mohamed Kamel <admin@cvrest.com>
2024-01-24 15:27:37 +0200

    filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers, models, permissions
@@ -112,6 +112,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentocation_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
     
     # def get_queryset(self):
     #     """Return objects for the current authenticated user only."""
