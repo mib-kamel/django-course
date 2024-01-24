6c80db902c8ae98ab57e24835de415b52fe5c02d
Mohamed Kamel <admin@cvrest.com>
2024-01-24 13:34:42 +0200

    ViewSets!

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
+
+routers = DefaultRouter()
+routers.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(routers.urls))
 ]
